5659a2bf340107c874584740699adfe85514ea02
Blake Bourque <Techplex.Engineer@gmail.com>
2014-12-29 13:40:52 -0500

    Drivetrain complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,58 +11,61 @@
 angular
   
   .module('ngEasyjApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'ui.router',
-    'Filters'
+	'ngAnimate',
+	'ngCookies',
+	'ngResource',
+	'ngRoute',
+	'ngSanitize',
+	'ngTouch',
+	'ui.router',
+	'Filters'
   ])
   // .run(function($rootScope) {
   //     $rootScope.isIDE = false;
   // })
   .config(function ($stateProvider, $urlRouterProvider) {
 
-    $stateProvider
+	$stateProvider
 
-      .state('index', {
-        url: '',
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-      })
-      .state('about', {
-        url: '/about',
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-      })
+	  .state('index', {
+		url: '',
+		templateUrl: 'views/main.html',
+		controller: 'MainCtrl',
+	  })
+	  .state('about', {
+		url: '/about',
+		templateUrl: 'views/about.html',
+		controller: 'AboutCtrl',
+	  })
 
-      .state('wizard', {
-        url: '/wizard',
-        templateUrl: 'views/wiz/wizard.html',
-        controller: 'WizardCtrl',
-      })
+	  .state('wizard', {
+		url: '/wizard',
+		templateUrl: 'views/wiz/wizard.html',
+		controller: 'WizardCtrl',
+	  })
 
-      .state('wizard.steps', {
-        url: '/:step',
-        templateUrl: function ($stateParams) {
-            // console.log('step',$stateParams.step);
-            return 'views/wiz/wiz_'+$stateParams.step+'.html';
-        },
-      });
+	  .state('wizard.steps', {
+		url: '/:step',
+		templateUrl: function ($stateParams) {
+			// console.log('step',$stateParams.step);
+			return 'views/wiz/wiz_'+$stateParams.step+'.html';
+		},
+	  });
 
-      $urlRouterProvider.otherwise('/');
+	  $urlRouterProvider.otherwise('/');
   });
 
-String.prototype.padLeft = function (l, c) { return Array(l - this.length + 1).join(c || " ") + this }
+String.prototype.padLeft = function (l, c) {
+	return new Array(l - this.length + 1).join(c || ' ') + this;
+};
 angular.module('Filters', []).
-    filter('range', function () {
-        return function (input, min, max) {
-            min = parseInt(min); //Make string input int
-            max = parseInt(max);
-            for (var i = min; i < max; i++)
-                input.push(i.toString().padLeft(2, "0"));
-            return input;
-        };
-    });
+	filter('range', function () {
+		return function (input, min, max) {
+			min = parseInt(min); //Make string input int
+			max = parseInt(max);
+			for (var i = min; i < max; i++){
+				input.push(i);//.toString().padLeft(2, '0')
+		  }
+			return input;
+		};
+	});
--- app/scripts/controllers/wizard.js
+++ app/scripts/controllers/wizard.js
@@ -8,47 +8,69 @@
  * Controller of the ngEasyjApp
  */
 angular.module('ngEasyjApp')
-  .controller('WizardCtrl', function ($stateParams,$window,$scope) {
-  	console.log("Wiz");
-  	var wiz = this;
-  	//constants for the RoboRio controller
-  	wiz.brain = {
-  		numPWM: 10
-  	};
-  	wiz.controllers = {};
+	.controller('WizardCtrl', function ($stateParams,$window) {
+		var wiz = this;
+		//constants for the RoboRio controller
+		wiz.brain = {
+			numPWM: 10
+		};
+		wiz.controllers = {};
 
-  	if (!$stateParams.step) {
-  		$window.location.href = '#/wizard/1';
-  	}
-  	wiz.step = $stateParams.step || 1;
-  	console.log(wiz.step);
+		if (!$stateParams.step) {
+			$window.location.href = '#/wizard/1';
+		}
+		wiz.step = $stateParams.step || 1;
 
- 	wiz.isStep = function(s) {
-  		return s == wiz.step;
-  	}
+		wiz.isStep = function(s) {
+			return s == wiz.step;
+		};
 
-  	wiz.getClass = function(s) {
-  		var out = 'disabled';
-  		if (wiz.step > s){
-  			out = 'complete';
-  		} else if (wiz.step == s) {
-  			out = 'active';
-  		} else {
-  			out = 'disabled';
-  		}
-  		return out;
-  	}
-  	wiz.next = function() {
-  		wiz.step ++;
-  		$window.location.href = '#/wizard/'+wiz.step;
-  	}
-  	wiz.getControllers = function() {
-  		var out = [];
-  		for (var i = 0; i < wiz.numMotors; i++) {
-  			out.push({
+		wiz.getClass = function(s) {
+			var out = 'disabled';
+			if (wiz.step > s){
+				out = 'complete';
+			} else if (wiz.step == s) {
+				out = 'active';
+			} else {
+				out = 'disabled';
+			}
+			return out;
+		};
 
-  			});
-  		};
-  	}
+		wiz.next = function() {
+			wiz.step ++;
+			$window.location.href = '#/wizard/'+wiz.step;
+		};
+		wiz.getControllers = function() {
+			var out = [];
+			if (wiz.numMotors == 2) {
+				out = ['left','right'];
+			} else if (wiz.numMotors == 4) {
+				out = ['frontLeft','rearLeft','frontRight','rearRight'];
+			} else if (wiz.numMotors == 6) {
+				out = ['frontLeft','midLeft','rearLeft','frontRight','midRight','rearRight'];
+			}
+			return out;
+		};
+		wiz.isPADisabled = function(n) {
+			var out = false;
+			for (var property in wiz.controllers) {
+				if (wiz.controllers.hasOwnProperty(property)) {
+					// console.log(wiz.controllers[property] == n);
+					if (wiz.controllers[property] == n) {
+						out = true;
+						break;
+					}
+				}
+			}
+			// for (var i = 0; i < wiz.controllers.length; i++) {
+			// 	console.log(wiz.controllers[i]);
+			// 	if (wiz.controllers[i] == n)
+			// 		out = true;
+			// 		break;
+			// };
+			return out;
 
-  });
+		}
+
+	});
